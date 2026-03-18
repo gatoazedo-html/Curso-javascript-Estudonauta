539d063023186e4de9aa9a3887b9b85f89c56121
gato azedo <beatrizleal2002@gmail.com>
2026-03-18 13:50:30 -0300

    exercicio time de futebol
    exercicio proposto por mim sobre times cariocas, dependendo do input do usuário, a página aparece customizada

---
+++ exercicio time de futebol/indexjavas.js
@@ -0,0 +1,38 @@
+function clicar() {
+    var time = document.getElementById('time').value;
+    var timem = time.toLowerCase();
+    var res = document.getElementById('res');
+    var img = document.createElement('img')
+    img.setAttribute('id', 'foto')
+    switch (timem) {
+        case 'vasco':
+            res.innerHTML = 'Bem-vindo, vascaíno!'
+            document.body.style.background = '#0D0D0D'
+            img.setAttribute('src', 'vasco.png')
+            break
+        case 'flamengo':
+            res.innerHTML = 'Bem-vindo, flamenguista!'
+            document.body.style.background = '#C00009'
+            img.setAttribute('src', 'flamengo.png')
+            break
+        case 'botafogo':
+            res.innerHTML = 'Bem-vindo, botafoguense!'
+            document.body.style.background = '#0D0D0D'
+            img.setAttribute('src', 'botafogo.png')
+            break
+        case 'fluminense':
+            res.innerHTML = 'Bem-vindo, fluminense!'
+            document.body.style.background = '#9F022F'
+            img.setAttribute('src', 'fluminense.png')
+            break
+        default:
+            window.alert('[ERRO] Time inválido, tente novamente!')
+            break
+    }
+    res.appendChild(img)
+}
+
+/* comentarios
+1 - se no seu html tiver uma imagem src pode colocar apenas a var img = getelementbyid e nas alternativas o img.src = 'nome do arquivo.png' - VIDE EXERCICIO SITE 15
+2 - se no seu arquivo html n tiver, vc tera que criar var img = document.createElement('img')
+    img.setAttribute('id', 'foto') e nas opções colocar img.setattribute('src', 'nome do arquivo.png') e no final colocar o id.appendChild(img) - VIDE EXERCICIO SITE IDADE 16 */
